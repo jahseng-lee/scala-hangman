839e9d7cb20ce499c94b3f2edf518efc47b31555
Jah Seng Lee <jahseng.lee@gmail.com>
2017-12-09 12:41:23 +1300

    Move view logic into object

--- src/main/scala/controller/Hangman.scala
+++ src/main/scala/controller/Hangman.scala
@@ -1,19 +1,22 @@
 package controller
 
 import model._
+import view._
 
 object Hangman {
   def main(args: Array[String]): Unit = {
     val game = new HangmanGame
+    val view = new HangmanView
 
-    println("Welcome to Hangman!")
-    println("~" * 20)
+    view.printGreeting
 
     while(game.running) {
-      val input = readLine("Please enter a leter: ")
+      val input = view.askForInput
 
       if(game.validInput(input)) {
         System.exit(0)
+      } else {
+        view.printInvalidInputWarning(input)
       }
     }
   }
---
+++ src/main/scala/view/HangmanView.scala
@@ -0,0 +1,21 @@
+package view
+
+class HangmanView {
+  def printGreeting: Unit = {
+    println("Welcome to Hangman!")
+    printDivider
+  }
+
+  def printInvalidInputWarning(input: String): Unit = {
+    println(s"$input is not a valid guess.")
+    printDivider
+  }
+
+  private def printDivider: Unit = {
+    println("~" * 20)
+  }
+
+  def askForInput: String = {
+    readLine("Please enter a leter: ")
+  }
+}
